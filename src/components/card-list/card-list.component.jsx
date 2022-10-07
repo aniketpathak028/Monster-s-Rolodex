89d4acb0db3ac1a3c42051ee504ab533cce8eb5c
Aniket Pathak <aniketpathak028@gmail.com>
2022-10-07 10:57:13 +0530

    create card component and add styles for card and card-list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,7 @@
 import { Component } from "react";
+import Card from "../card/card.component";
+
+import './card-list.styles.css'
 
 class CardList extends Component {
     render() {
@@ -6,17 +9,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, id, email } = monster;
-            return (
-              <div key={id} className="card-container">
-                <img
-                  alt={`monster ${name}`}
-                  src={`https://robohash.org/${id}?set=set2`}
-                />
-                <h2>{name}</h2>
-                <p>{email}</p>
-              </div>
-            );
+            return <Card monster={monster} />;
           })}
       </div>
     );
