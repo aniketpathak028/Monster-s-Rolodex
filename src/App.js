8f28cd98d54693150bf56effcccfbd90eec64eaa
Aniket Pathak <aniketpathak028@gmail.com>
2022-10-07 20:09:28 +0530

    transform App.js from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,110 @@
-import { Component } from "react"; // importing Component class from react library
+import { Component } from "react";
+import { useState, useEffect } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
-// import components
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  // the constructor is invoked first whenever the class is instantiated
-  constructor() {
-    super(); // call super method
-    // defines the state of the component
-    this.state = {
-      monsters: [],
-      searchField: "", // initially the searchField is empty, hence all monsters are displayed
-    };
-  }
+// Functional-Component
+const App = () => {
+  console.log("render");
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  // lifecycle method that runs when the component first mounts
-  componentDidMount() {
-    // native fetch that returns a promise on success
+  // fetches the api data only when the app is mounted for the first time
+  useEffect(() => {
+    console.log("event firing");
     fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json()) // transforms response into json
+      .then((response) => response.json())
       .then((users) => {
-        // setState method is used to change the state of the component asynchronously
-        this.setState({
-          monsters: users,
-        });
+        setMonsters(users);
       });
-  }
+  }, [setMonsters]);
 
-  // define the onChange handler for input outside the render method to optimize performance
-  onChangeHandler = (event) => {
-    // convert the input text into lowercase
-    const searchField = event.target.value.toLocaleLowerCase();
-    // change the searchString property to re-render the component
-    this.setState(() => {
-      return {
-        searchField,
-      };
-    });
+  useEffect(() => {
+     setFilteredMonsters(monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    }))
+  }, [monsters, searchField]);
+
+  const onChangeHandler = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  // render method, renders the JSX immediatedly after constructor is called
-  render() {
-    // destructure state properties (code-readability)
-    const { monsters, searchField } = this.state;
-    const { onChangeHandler } = this;
-
-    // creating a new list of filtered monsters based on the input-text
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox className= "search-box" placeholder= "search monsters" onChangeHandler= {onChangeHandler} />
-        <CardList monsters= {filteredMonsters}/>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="search-box"
+        placeholder="search monsters"
+        onChangeHandler={onChangeHandler}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
+
+// Class-Component
+
+// class App extends Component {
+//   // the constructor is invoked first whenever the class is instantiated
+//   constructor() {
+//     super(); // call super method
+//     // defines the state of the component
+//     this.state = {
+//       monsters: [],
+//       searchField: "", // initially the searchField is empty, hence all monsters are displayed
+//     };
+//   }
+
+//   // lifecycle method that runs when the component first mounts
+//   componentDidMount() {
+//     // native fetch that returns a promise on success
+//     fetch(`https://jsonplaceholder.typicode.com/users`)
+//       .then((response) => response.json()) // transforms response into json
+//       .then((users) => {
+//         // setState method is used to change the state of the component asynchronously
+//         this.setState({
+//           monsters: users,
+//         });
+//       });
+//   }
+
+//   // define the onChange handler for input outside the render method to optimize performance
+//   onChangeHandler = (event) => {
+//     // convert the input text into lowercase
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     // change the searchString property to re-render the component
+//     this.setState(() => {
+//       return {
+//         searchField,
+//       };
+//     });
+//   };
+
+//   // render method, renders the JSX immediatedly after constructor is called
+//   render() {
+//     // destructure state properties (code-readability)
+//     const { monsters, searchField } = this.state;
+//     const { onChangeHandler } = this;
+
+//     // creating a new list of filtered monsters based on the input-text
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox className= "search-box" placeholder= "search monsters" onChangeHandler= {onChangeHandler} />
+//         <CardList monsters= {filteredMonsters}/>
+//       </div>
+//     );
+//   }
+// }
